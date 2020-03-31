67576f019bc9056f093797270bf4477358bc83d6
José Manuel Lucas <josemanuel@jmlweb.es>
2020-04-01 01:21:38 +0200

    feat: Include rejected requests

---
+++ src/scenes/Dashboard/DataTable.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { Box, Flex } from '@chakra-ui/core';
+import PT from 'prop-types';
+
+import { CommonLink } from '../../components';
+import DateTime from './DateTime';
+import PriorityTag from './PriorityTag';
+import { THead, TBody, Tr, Th, Td } from './styled';
+import StatusesSelector from './StatusesSelector';
+import { REQUEST_STATUSES } from '../../constants';
+
+const FORBIDEN_STATUSES = ['cancelled', 'rejected', 'sent'];
+
+const mustShow = (status, showInactive) =>
+  FORBIDEN_STATUSES.includes(status) === showInactive;
+
+const DataTable = ({
+  materialRequests,
+  createStatusUpdateFn,
+  showInactive,
+}) => (
+  <Flex
+    flexDirection="column"
+    flex={1}
+    maxWidth="100%"
+    width="100%"
+    overflowX="auto"
+    borderRadius={4}
+    border="1px"
+    borderColor="gray.200"
+    boxShadow="0 3px 8px rgba(0, 0, 0, 0.1)"
+  >
+    <Box as="table" width="full" table-layout="auto" border-collapse="collapse">
+      <THead>
+        <Tr>
+          <Th>Fecha</Th>
+          <Th>Nombre</Th>
+          <Th>Email</Th>
+          <Th>Teléfono</Th>
+          <Th>Centro</Th>
+          <Th textAlign="center">Cantidad</Th>
+          <Th textAlign="center">Prioridad</Th>
+          <Th textAlign="center">Estado</Th>
+        </Tr>
+      </THead>
+      <TBody>
+        {materialRequests.map(
+          (item) =>
+            mustShow(item.status, showInactive) && (
+              <Tr key={item.id}>
+                <Td fontSize="sm">
+                  <DateTime seconds={item.createdAt.seconds} />
+                </Td>
+                <Td>{item.name}</Td>
+                <Td fontSize="sm">
+                  <CommonLink
+                    title="Enviar email"
+                    href={`mailto:${item.email}`}
+                  >
+                    {item.email}
+                  </CommonLink>
+                </Td>
+                <Td fontSize="sm">{item.phone}</Td>
+                <Td>{item.org}</Td>
+                <Td textAlign="center">{item.qty}</Td>
+                <Td textAlign="center">
+                  <PriorityTag priority={item.priority} />
+                </Td>
+                <Td textAlign="center">
+                  <StatusesSelector
+                    status={item.status}
+                    updateFn={createStatusUpdateFn(item.id)}
+                  />
+                </Td>
+              </Tr>
+            ),
+        )}
+      </TBody>
+    </Box>
+  </Flex>
+);
+
+DataTable.defaultProps = {
+  showInactive: false,
+};
+
+DataTable.propTypes = {
+  materialRequests: PT.arrayOf(
+    PT.shape({
+      status: PT.oneOf(Object.keys(REQUEST_STATUSES)),
+    }),
+  ).isRequired,
+  createStatusUpdateFn: PT.func.isRequired,
+  showInactive: PT.bool,
+};
+
+export default DataTable;
--- src/scenes/Dashboard/index.js
+++ src/scenes/Dashboard/index.js
@@ -1,15 +1,10 @@
 import React, { useMemo } from 'react';
 import { useFirestore, useFirestoreCollection } from 'reactfire';
-import { Box, Flex } from '@chakra-ui/core';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/core';
 
-import { CommonLink, Container, MainLayout } from '../../components';
+import { Container, MainLayout } from '../../components';
 import { useAlertNotification } from '../../hooks';
-import DateTime from './DateTime';
-import PriorityTag from './PriorityTag';
-import { THead, TBody, Tr, Th, Td } from './styled';
-import StatusesSelector from './StatusesSelector';
-
-const FORBIDEN_STATUSES = ['cancelled', 'rejected', 'sent'];
+import DataTable from './DataTable';
 
 const Dashboard = () => {
   const firestore = useFirestore();
@@ -17,17 +12,15 @@ const Dashboard = () => {
     firestore
       .collection('material-requests')
       .orderBy('createdAt', 'desc')
-      .limit(999),
+      .limit(100),
   );
   const materialRequests = useMemo(
     () =>
-      docs
-        .map((item) => ({
-          id: item.id,
-          ...item.data(),
-          update: item.update,
-        }))
-        .filter((item) => !FORBIDEN_STATUSES.includes(item.status)),
+      docs.map((item) => ({
+        id: item.id,
+        ...item.data(),
+        update: item.update,
+      })),
     [docs],
   );
   const notify = useAlertNotification({
@@ -48,67 +41,27 @@ const Dashboard = () => {
   return (
     <MainLayout>
       <Container>
-        <Flex
-          flexDirection="column"
-          flex={1}
-          maxWidth="100%"
-          width="100%"
-          overflowX="auto"
-          borderRadius={4}
-          border="1px"
-          borderColor="gray.200"
-          boxShadow="0 3px 8px rgba(0, 0, 0, 0.1)"
-        >
-          <Box
-            as="table"
-            width="full"
-            table-layout="auto"
-            border-collapse="collapse"
-          >
-            <THead>
-              <Tr>
-                <Th>Fecha</Th>
-                <Th>Nombre</Th>
-                <Th>Email</Th>
-                <Th>Teléfono</Th>
-                <Th>Centro</Th>
-                <Th textAlign="center">Cantidad</Th>
-                <Th textAlign="center">Prioridad</Th>
-                <Th textAlign="center">Estado</Th>
-              </Tr>
-            </THead>
-            <TBody>
-              {materialRequests.map((item) => (
-                <Tr key={`${item.uid}${item.createdAt.seconds}`}>
-                  <Td fontSize="sm">
-                    <DateTime seconds={item.createdAt.seconds} />
-                  </Td>
-                  <Td>{item.name}</Td>
-                  <Td fontSize="sm">
-                    <CommonLink
-                      title="Enviar email"
-                      href={`mailto:${item.email}`}
-                    >
-                      {item.email}
-                    </CommonLink>
-                  </Td>
-                  <Td fontSize="sm">{item.phone}</Td>
-                  <Td>{item.org}</Td>
-                  <Td textAlign="center">{item.qty}</Td>
-                  <Td textAlign="center">
-                    <PriorityTag priority={item.priority} />
-                  </Td>
-                  <Td textAlign="center">
-                    <StatusesSelector
-                      status={item.status}
-                      updateFn={createStatusUpdateFn(item.id)}
-                    />
-                  </Td>
-                </Tr>
-              ))}
-            </TBody>
-          </Box>
-        </Flex>
+        <Tabs variant="soft-rounded" variantColor="teal">
+          <TabList mb={4}>
+            <Tab>Activos</Tab>
+            <Tab>Inactivos</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <DataTable
+                materialRequests={materialRequests}
+                createStatusUpdateFn={createStatusUpdateFn}
+              />
+            </TabPanel>
+            <TabPanel>
+              <DataTable
+                materialRequests={materialRequests}
+                createStatusUpdateFn={createStatusUpdateFn}
+                showInactive
+              />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Container>
     </MainLayout>
   );
