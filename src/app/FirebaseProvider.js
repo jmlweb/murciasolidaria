a9ef3d21f4b5914ada6ffb20080f2a8c0e585146
José Manuel Lucas <josemanuel@jmlweb.es>
2020-04-01 09:20:48 +0200

    fix: Measurement id typo

--- src/app/FirebaseProvider.js
+++ src/app/FirebaseProvider.js
@@ -10,7 +10,7 @@ const config = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const FirebaseProvider = ({ children }) => (
