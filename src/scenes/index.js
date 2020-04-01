e5d2834df9e38a8326d1bd0a1ad19f1f71934f37
José Manuel Lucas <josemanuel@jmlweb.es>
2020-04-01 21:29:26 +0200

    fix: Rules

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -1,33 +1,43 @@
 import React, { Suspense, lazy, memo } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Flex, Spinner } from '@chakra-ui/core';
+import { preloadAuth, preloadFirestore } from 'reactfire';
 
 const Dashboard = lazy(() => import('./Dashboard'));
 const Home = lazy(() => import('./Home'));
 const RequestMaterial = lazy(() => import('./RequestMaterial'));
 
-const Scenes = () => (
-  <Suspense
-    fallback={
-      <Flex justifyContent="center" alignItems="center" height="100vh">
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="cyan.200"
-          color="cyan.500"
-          size="xl"
-        />
-      </Flex>
-    }
-  >
-    <Router>
-      <Switch>
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/request-material" component={RequestMaterial} />
-        <Route component={Home} />
-      </Switch>
-    </Router>
-  </Suspense>
-);
+const Scenes = () => {
+  preloadAuth();
+  preloadFirestore({
+    setup: (firestore) =>
+      firestore().enablePersistence({
+        synchronizeTabs: true,
+      }),
+  });
+  return (
+    <Suspense
+      fallback={
+        <Flex justifyContent="center" alignItems="center" height="100vh">
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="cyan.200"
+            color="cyan.500"
+            size="xl"
+          />
+        </Flex>
+      }
+    >
+      <Router>
+        <Switch>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/request-material" component={RequestMaterial} />
+          <Route component={Home} />
+        </Switch>
+      </Router>
+    </Suspense>
+  );
+};
 
 export default memo(Scenes);
