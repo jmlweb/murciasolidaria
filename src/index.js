aa82ad9c3f3e8b4ea665a8e9425752cf3342979d
José Manuel Lucas <josemanuel@jmlweb.es>
2020-04-01 20:46:20 +0200

    feat: Disable sw

--- src/index.js
+++ src/index.js
@@ -13,8 +13,9 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-if (process.env.NODE_ENV === 'production') {
-  serviceWorker.register();
-} else {
-  serviceWorker.unregister();
-}
+// if (process.env.NODE_ENV === 'production') {
+//   serviceWorker.register();
+// } else {
+//   serviceWorker.unregister();
+// }
+serviceWorker.unregister();
