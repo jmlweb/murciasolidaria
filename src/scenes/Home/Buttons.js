5921dbf162aa1cd70a633980753152574445a1fb
José Manuel Lucas <josemanuel@jmlweb.es>
2020-04-02 17:54:20 +0200

    fix: Donation links, buttons max-width

--- src/scenes/Home/Buttons.js
+++ src/scenes/Home/Buttons.js
@@ -4,28 +4,31 @@ import { Link as ReactRouterLink } from 'react-router-dom';
 import { FiHeart, FiTruck } from 'react-icons/fi';
 
 import { ROUTES } from '../../constants';
+import { Container } from '../../components';
 
 const Buttons = () => (
-  <Stack isInline justifyContent="center" mt={4} mb={10} spacing={8}>
-    <Button
-      size="lg"
-      as={ReactRouterLink}
-      to={ROUTES.requestMaterial}
-      variantColor="red"
-    >
-      <Box as={FiHeart} display="inline-block" mr={1} />
-      Donaciones
-    </Button>
-    <Button
-      size="lg"
-      as={ReactRouterLink}
-      to={ROUTES.requestMaterial}
-      variantColor="teal"
-    >
-      <Box as={FiTruck} display="inline-block" mr={1} />
-      Solicitar pantallas
-    </Button>
-  </Stack>
+  <Container>
+    <Stack isInline justifyContent="center" mt={4} mb={10} spacing={[2, 6, 8]}>
+      <Button
+        size="lg"
+        as={ReactRouterLink}
+        to={ROUTES.donations}
+        variantColor="red"
+      >
+        <Box as={FiHeart} display="inline-block" mr={1} />
+        Donaciones
+      </Button>
+      <Button
+        size="lg"
+        as={ReactRouterLink}
+        to={ROUTES.requestMaterial}
+        variantColor="teal"
+      >
+        <Box as={FiTruck} display="inline-block" mr={1} />
+        Solicitar pantallas
+      </Button>
+    </Stack>
+  </Container>
 );
 
 export default memo(Buttons);
